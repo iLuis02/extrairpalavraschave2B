cea8676045a26b6843c632f2ceda4f9fc91fd6d1
iLuis02 <lerio.rodrigues@escola.pr.gov.br>
2025-09-15 08:01:13 -0300

    15.09.2025

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function mostraPalavrasChave () {
 
     campoResultado.textContent = palavrasChave.join(",");
 }
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+
+    for(let i in palavras){
+        palavras [i] =palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
+    const frequencias = contaFrequencia(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenaPalavra(p1,p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    
+}
